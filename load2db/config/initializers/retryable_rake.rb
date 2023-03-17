7e602b3e613e469fa272d8f1e748c58150936467
Hooopo <Hoooopo@gmail.com>
2023-03-18 01:16:32 +0800

    Update retryable_rake.rb

--- load2db/config/initializers/retryable_rake.rb
+++ load2db/config/initializers/retryable_rake.rb
@@ -5,14 +5,14 @@ require 'retryable'
 
 class RetryableRake 
   def self.db_migrate 
-    Retryable.retryable(tries: 3, matching: /Lost connection to MySQL server/) do |retries, exception|
+    Retryable.retryable(tries: 10, matching: /Lost connection to MySQL server/) do |retries, exception|
       puts "Retrying #{retries} times due to #{exception}" if retries > 0
       Rake::Task['db:migrate'].invoke
     end
   end
 
   def self.db_create 
-    Retryable.retryable(tries: 3, matching: /Lost connection to MySQL server/) do |retries, exception|
+    Retryable.retryable(tries: 10, matching: /Lost connection to MySQL server/) do |retries, exception|
       puts "Retrying #{retries} times due to #{exception}" if retries > 0
       Rake::Task['db:create'].invoke
     end
